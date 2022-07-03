8b233585b743c6353295c32ff2da1cb17143d0eb
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2022-07-03 00:09:56 -0500

    Added NTP, removed old method

--- src/main.cpp
+++ src/main.cpp
@@ -7,70 +7,38 @@
 // Use this for Serial communication, comment for silent operation
 #define VERBOSE
 
-// Please tune the following value if the clock gains or loses.
-// Theoretically, standard of this value is 60000.
-#define MILLIS_PER_MIN 60000 // milliseconds per a minute
+WiFiManager wm;
+bool wm_nonblocking = false; //change if this causes issues
 
-// Motor and clock parameters
-// 4096 * 110 / 8 = 56320
-#define STEPS_PER_ROTATION        56320 // steps for a full turn of minute rotor
-#define STEPS_PER_MINUTE          STEPS_PER_ROTATION / 60
-#define STEPPER_SPEED             400   // steps/s
-#define STEPPER_ACCEL             400   // steps/s2
+// Clock parameters
+  // 4096 * 110 / 8 = 56320
+  #define STEPS_PER_ROTATION        56320 // steps for a full turn of minute rotor
+  #define STEPS_PER_MINUTE          STEPS_PER_ROTATION / 60
 
-// wait for a single step of stepper
-int delaytime = 1;
-
-// ports used to control the stepper motor
-// if your motor rotate to the opposite direction, 
-// change the order as {7, 6, 5, 4};
-//int port[4] = {14, 12, 13, 15};
-#define HALFSTEP 8
-#define FULLSTEP 4
-AccelStepper stepper(HALFSTEP, D5, D7, D6, D8);
+// Motors
+  #define STEPPER_SPEED             400   // steps/s
+  #define STEPPER_ACCEL             400   // steps/s2
+  #define HALFSTEP 8
+  #define FULLSTEP 4
+  AccelStepper stepper(HALFSTEP, D5, D7, D6, D8);
 
 // Buttons
-#define BUTTON1      D3     // forward
-#define BUTTON2      D2     // Reverse
-Button2 button_fwd;
-Button2 button_rvs;
+  #define BUTTON1      D3     // forward
+  #define BUTTON2      D2     // Reverse
+  Button2 button_fwd;
+  Button2 button_rvs;
 
-/*
-// sequence of stepper motor control
-
-int seq[8][4] = {
-  {  LOW, HIGH, HIGH,  LOW},
-  {  LOW,  LOW, HIGH,  LOW},
-  {  LOW,  LOW, HIGH, HIGH},
-  {  LOW,  LOW,  LOW, HIGH},
-  { HIGH,  LOW,  LOW, HIGH},
-  { HIGH,  LOW,  LOW,  LOW},
-  { HIGH, HIGH,  LOW,  LOW},
-  {  LOW, HIGH,  LOW,  LOW}
-};
+// Time Keeping (NTP)
+  long utcOffsetInSeconds_DST  = -18000;
+  long utcOffsetInSeconds      = -21600;
+  #define NTP_UPDATE_INT         900000
+  #define NTP_SERVER             "north-america.pool.ntp.org"
+  int time_min_prev = 0;
+  int time_min = 0;
 
 
-void rotate(int step) {
-  static int phase = 0;
-  int i, j;
-  int delta = (step > 0) ? 1 : 7;
-  int dt = 20;
-
-  step = (step > 0) ? step : -step;
-  for(j = 0; j < step; j++) {
-    phase = (phase + delta) % 8;
-    for(i = 0; i < 4; i++) {
-      digitalWrite(port[i], seq[phase][i]);
-    }
-    delay(dt);
-    if(dt > delaytime) dt--;
-  }
-  // power cut
-  for(i = 0; i < 4; i++) {
-    digitalWrite(port[i], LOW);
-  }
-}
-*/
+WiFiUDP ntpUDP;
+NTP ntp(ntpUDP);
 
 void makeMove(int steps) {
   #ifdef VERBOSE
@@ -122,12 +90,44 @@ void setup() {
     Serial.println("Starting program");
   #endif
 
+  // Setup WiFi
+  #ifdef VERBOSE
+    Serial.println("Setting up WiFi");
+  #endif
+    WiFi.mode(WIFI_STA);
+
+    // WiFiManager Parameters
+    wm.setConnectTimeout(20);
+    wm.setConfigPortalTimeout(300);
+
+    bool res;
+    res = wm.autoConnect("HollowClock3"); 
+    if (res) {
+        Serial.println("WiFi Connected Successfully!");
+    } else {
+        Serial.println("Failed to Connect WiFi!");
+        ESP.restart();
+    }
+
+  // Setup NTP
+  #ifdef VERBOSE
+    Serial.println("Setting up NTP");
+  #endif
+  ntp.updateInterval(900000); // set to update from ntp server every 900 seconds, or 15 minutes
+  ntp.ruleDST("CDT", Second, Sun, Mar, 2, -300);
+  ntp.ruleSTD("CST",  First, Sun, Nov, 3, -360);
+  ntp.begin(NTP_SERVER);
+  time_min_prev = ntp.minutes();
+  time_min = ntp.minutes();
+
+  // Setup Motors
   #ifdef VERBOSE
     Serial.println("Setting up motor");
   #endif
   stepper.setMaxSpeed(STEPPER_SPEED);
   stepper.setAcceleration(STEPPER_ACCEL);
 
+  // Setup Buttons
   #ifdef VERBOSE
     Serial.println("Setting up buttons");
   #endif
@@ -157,21 +157,23 @@ void loop() {
 
   } else {
     // Run based on time
-    static long prev_min = 0, prev_pos = 0;
-    long min;
-    static long pos;
+    ntp.update();
     
-    min = millis() / MILLIS_PER_MIN;
-    if(prev_min == min) {
+    time_min = ntp.minutes();
+
+    if(time_min_prev == time_min) {
       return;
     }
-    prev_min = min;
-    pos = STEPS_PER_MINUTE * min;
-    // add one extra steps every 2 out of 3 minutes (will be easier when comparing minutes from NTP)
-    makeMove(pos - prev_pos);
-    prev_pos = pos;
     #ifdef VERBOSE
-      Serial.println("rotated a minute");
+      Serial.print("Minute: "); Serial.println(time_min);
     #endif
+    
+    int pos = STEPS_PER_MINUTE * (time_min - time_min_prev);
+    // add one extra steps every 2 out of 3 minutes
+    if (time_min % 3 > 0) {
+      pos = pos + 1;
+    }
+    makeMove(pos);
+    time_min_prev = time_min;
   }
 }
